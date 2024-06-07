e5a977c898358a13b955c6d1839f4f90c04538cf
Frazer Rose <frazerjamesrose@outlook.com>
2024-06-07 20:47:38 +0100

    Make it more obvious how the default works
    Make it more obvious how to use the default so my less tech-minded colleagues will understand

--- PyGameInstaller.py
+++ PyGameInstaller.py
@@ -1,7 +1,7 @@
 import os
 
 base_command = 'py -m pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org '
-package = input("What package would you like to install? (Default is pygame) ")
+package = input("What package would you like to install? (Just press Enter for pygame) ")
 
 if package:
     install_command = base_command + package
