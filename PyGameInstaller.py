04292171d53aabc0c12f2f275984ebc4a8920f7b
Frazer Rose <frazerjamesrose@outlook.com>
2024-06-07 22:04:29 +0100

    Reformat code to meet PEP 8 standard E501

--- PyGameInstaller.py
+++ PyGameInstaller.py
@@ -1,6 +1,10 @@
 import os
 
-base_command = 'py -m pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org '
+base_command = 'py -m pip install \
+--trusted-host pypi.org \
+--trusted-host pypi.python.org \
+--trusted-host files.pythonhosted.org '
+
 package = input("What package would you like to install? (Just press Enter for pygame) ")
 
 if package:
