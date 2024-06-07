9d1ac75aede7cdfc54ff4c742c2321c8f67aff24
Frazer Rose <frazerjamesrose@outlook.com>
2024-06-07 20:32:15 +0100

    Initial commit

---
+++ PyGameInstaller.py
@@ -0,0 +1,11 @@
+import os
+
+base_command = 'py -m pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org '
+package = input("What package would you like to install? (Default is pygame) ")
+
+if package:
+    install_command = base_command + package
+else:
+    install_command = base_command + 'pygame'
+
+os.system(install_command)
